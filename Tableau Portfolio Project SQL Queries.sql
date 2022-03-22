c83e5dd665137323983b2efb8e36c3e6b3c65b9e
Adam-Sosnowski <100883462+Adam-Sosnowski@users.noreply.github.com>
2022-03-22 18:13:49 +0000

    Update Tableau Portfolio Project SQL Queries.sql

--- Tableau Portfolio Project SQL Queries.sql
+++ Tableau Portfolio Project SQL Queries.sql
@@ -9,7 +9,7 @@ Queries used for Tableau Covid project
 -- 1. 
 
 Select SUM(new_cases) as total_cases, SUM(CAST(new_deaths AS int)) AS total_deaths, SUM(CAST(new_deaths AS int))/SUM(new_cases)*100 AS DeathPercentage
-From PortfolioProject..CovidDeaths
+From PortfolioProject..Covid_Deaths
 Where continent is not null 
 -- Group By date
 Order by 1,2
@@ -22,7 +22,7 @@ Order by 1,2
 -- European Union is part of Europe
 
 Select location, SUM(CAST(new_deaths AS int)) AS TotalDeathCount
-From PortfolioProject..CovidDeaths
+From PortfolioProject..Covid_Deaths
 Where continent is null 
 and location not in ('World', 'European Union', 'International')
 Group by location
@@ -33,7 +33,7 @@ Order by TotalDeathCount DESC
 -- 3.
 
 Select location, population, MAX(total_cases) AS HighestInfectionCount,  MAX((total_cases/population))*100 AS PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From PortfolioProject..Covid_Deaths
 Group by location, population
 Order by PercentPopulationInfected DESC
 
@@ -42,6 +42,6 @@ Order by PercentPopulationInfected DESC
 -- 4.
 
 Select location, population, date, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population))*100 AS PercentPopulationInfected
-From PortfolioProject..CovidDeaths
+From PortfolioProject..Covid_Deaths
 Group by location, population, date
 Order by PercentPopulationInfected DESC
